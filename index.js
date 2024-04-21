4e920479f63317a25ff60ea171e75106093cc133
Al-bashir <albashirabdi482@gmail.com>
2024-04-21 13:16:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let Main=document.querySelector('main');
+Main.remove();
+
+let newHeader=document.createElement('h1');
+
+newHeader.id='victory';
+
+newHeader.textContent='Alba is the champion';
